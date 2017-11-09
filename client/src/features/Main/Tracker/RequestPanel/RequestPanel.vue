d8dabd88cb93a498101f99f6d8cead70e48a54b1
Thiago Yoithi <thyoity@gmail.com>
2017-11-09 08:40:39 -0200

    Some mouse event updates on RequestPanel.

--- client/src/features/Main/Tracker/RequestPanel/RequestPanel.vue
+++ client/src/features/Main/Tracker/RequestPanel/RequestPanel.vue
@@ -1,8 +1,10 @@
 <template>
-    <div id="request-panel">
+    <div id="request-panel" ref="requestPanel">
         <div class="board">
-            <div class="board-section" v-for="(boardSection, index) in boardSections" :style="{ width: (boardSection.size * boardOptions.columnWidth) + 'px' }" :key="index">
-                <div class="board-section-header">
+            <div class="board-section" v-for="(boardSection, index) in boardSections" ref="boardSections"
+                 :style="{ width: (boardSection.size * boardOptions.columnWidth) + 'px' }" :key="index" draggable="true"
+                 @mousedown="onBoardSectionMouseDown($event)" @dragstart="onBoardSectionHeaderDrag($event)" @dragend="onBoardSectionHeaderDragEnd($event)" >
+                <div class="board-section-header" >
                     <div class="summary">
                         <span>0 pedidos nesta coluna</span>
                     </div>
@@ -10,13 +12,14 @@
                         <h3>{{ boardSection.name }}</h3>
                         <span class="push-both-sides"></span>
                         <ul>
-                            <li>Expandir</li>
-                            <li>Diminuir</li>
+                            <li>E</li>
+                            <li>D</li>
                         </ul>
                     </div>
                 </div>
-                <div class="board-cards">
+                <div class="board-section-cards">
                     <div class="board-card">
+                        sadasd
                     </div>
                 </div>
             </div>
@@ -46,7 +49,8 @@
                 ],
                 boardOptions: {
                     columnWidth: 262
-                }
+                },
+                currentMouseDownTarget: false
             }
         },
         computed: {
@@ -55,6 +59,37 @@
             ])
         },
         methods: {
+            onBoardSectionMouseDown(ev){
+                this.currentMouseDownTarget = ev.target;
+            },
+            onBoardSectionHeaderDrag(ev){
+                const vm = this;
+                const isDraggingBoardSection = _.some(this.$refs.boardSections, (boardSectionEl) => {
+                    const el = boardSectionEl.querySelector(".board-section-header");
+                    if(el.contains(vm.currentMouseDownTarget)){
+                        return true;
+                    }
+                });
+                if(isDraggingBoardSection){
+                    ev.dataTransfer.setData('text/plain', 'handle');
+                    ev.target.style.opacity = 0.3;
+                }
+                else {
+                    ev.preventDefault();
+                }
+            },
+            onBoardSectionHeaderDragEnd(ev){
+                ev.target.style.opacity = 1;
+            }
+        },
+        mounted(){
+            /*const boardSectionHeaderEls = this.$refs.requestPanel.querySelectorAll(".board .board-section .board-section-header");
+            if(boardSectionHeaderEls.length > 0) {
+                boardSectionHeaderEls.forEach((boardSectionHeaderEl) => {
+                    const boardSectionEl = boardSectionHeaderEl.parentElement;
+                    boardSectionHeaderEl
+                });
+            }*/
         }
     }
 </script>
@@ -68,18 +103,55 @@
         overflow: auto;
     }
     #request-panel > .board {
-        padding: 10px;
         margin: 10px;
         display: flex;
         flex-direction: row;
     }
     #request-panel > .board > .board-section {
-        margin: 0 10px;
+        margin: 0 10px 0 0;
+        padding: 10px 0;
+        background: #26272E;
+    }
+    #request-panel > .board > .board-section[draggable] {
+        -moz-user-select: none;
+        -webkit-user-select: none;
+        user-select: none;
+        /* Required to make elements draggable in old WebKit */
+        -webkit-user-drag: element;
     }
     #request-panel > .board > .board-section:first-child {
         margin-left: 0;
     }
     #request-panel > .board > .board-section:last-child {
         margin-right: 0;
+    }
+    #request-panel > .board > .board-section > .board-section-header {
+        cursor: -webkit-grab;
+        cursor: -moz-grab;
+        cursor: grab;
+        padding: 0 10px;
+        padding-bottom: 8px;
+        margin-bottom: 8px;
+        border-bottom: 1px solid #222;
+    }
+    #request-panel > .board > .board-section > .board-section-header > .title-section {
+        display: flex;
+        flex-direction: row;
+        align-items: center;
+        justify-content: center;
+    }
+    #request-panel > .board > .board-section > .board-section-header > .title-section ul li {
+        display: inline;
+        cursor: pointer;
+        position: relative;
+        z-index: 1000;
+    }
+    #request-panel > .board > .board-section.grabbing > .board-section-header {
+        cursor: -webkit-grabbing;
+        cursor: -moz-grabbing;
+        cursor: grabbing;
+    }
+    #request-panel > .board > .board-section > .board-section-cards {
+        padding: 0 10px;
     }
 </style>
