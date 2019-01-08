b9f31c947e341fe7fd3ded955ca8f89c1649baa0
Felipe henrique <win2016_br@outlook.com>
2019-01-08 13:55:49 -0200

    TopBanner created

---
+++ ds/src/patterns/TopBanner.vue
@@ -0,0 +1,119 @@
+<template>
+  <component :is="type" class="index">
+    <Heading>Lorem ipsum sit amet arem ipsum sit amet'</Heading>
+
+    <component :is="type" class="block1">
+      <Wrapper>
+        <Paragraph class="paragraph1">
+          Lorem ipsum dolor sit amet, consectetur adipiscing.
+        </Paragraph>
+        <Paragraph class="paragraph2">
+          Duis aute irure dolor in reprehenderit<br />in voluptate velit esse cillum dolore eu<br />fugiat
+          nulla pariatur. Excepteur sint<br />accaecat cupidatat
+        </Paragraph>
+        <Button variation="primary" size="large">Primary Button</Button>
+      </Wrapper>
+    </component>
+
+    <component :is="type" class="block2">
+      <Wrapper> <img src="" /> </Wrapper>
+
+      <component :is="type" class="block3">
+        <Wrapper> <img src="" /> </Wrapper>
+      </component>
+    </component>
+  </component>
+</template>
+
+<script>
+/**
+ * Shows how to layout and structure a home page.
+ */
+export default {
+  name: "TopBanner",
+  status: "deprecated",
+  release: "1.0.0",
+  metaInfo: {
+    title: "GestaON DS",
+    htmlAttrs: {
+      lang: "en",
+    },
+  },
+  props: {
+    /**
+     * The html element name used for the component.
+     */
+    type: {
+      type: String,
+      default: "div",
+    },
+  },
+}
+</script>
+
+<style lang="scss" scoped>
+// Design Tokens with local scope
+$color-template-background: $color-black;
+$color-template-background-top: tint($color-template-background, 10%);
+$color-template-background-bottom: shade($color-template-background, 5%);
+$color-template-text: $color-white;
+$color-template-link: $color-accent-3;
+
+.index {
+  @include reset;
+  @include inset-space($space-m);
+  min-height: $space-xxl * 4;
+  background: $color-base-10;
+  position: relative;
+  float: left;
+  height: 100%;
+  width: 100%;
+  @media #{$media-query-l} {
+    // This is how you’d use design tokens with media queries
+  }
+  .heading {
+    color: $color-primary-50;
+    font-size: $size-xl;
+    text-align: center;
+    margin-top: $size-xl;
+  }
+  .paragraph1 {
+    color: rgb(0, 107, 139);
+    margin-bottom: 8px;
+    font-size: $size-s;
+  }
+  .paragraph2 {
+    color: rgb(0, 139, 139);
+    margin-bottom: 8px;
+  }
+  .text-link {
+    color: $color-template-link;
+  }
+  .wrapper {
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    max-width: $space-xxl * 4.5;
+    transform: translateX(-50%) translateY(-50%);
+    position: absolute;
+    left: 30%;
+    top: 50%;
+  }
+}
+a {
+  font-family: $font-text;
+  color: $color-primary;
+  text-decoration: underline;
+}
+.active {
+  color: $color-base-40;
+}
+.button {
+  margin-top: $size-m;
+}
+</style>
+
+<docs>
+  ```jsx
+  <TopBanner />
+  ```
+</docs>
