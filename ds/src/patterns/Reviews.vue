c1ec982816e6fd0556797b06fadd946f93fa2f39
Carlos Elias <carlos.elias.10@hotmail.com>
2019-01-17 17:19:56 -0200

    Changes NavBar and created the pattern Reviews.

---
+++ ds/src/patterns/Reviews.vue
@@ -0,0 +1,22 @@
+<script>
+/**
+ * Used as main page navigation in templates.
+ */
+export default {
+  name: "Reviews",
+  status: "ready",
+  release: "1.0.0",
+  model: {
+    prop: "active",
+  },
+  props: {
+    /**
+     * The html element name used for the nav bar.
+     */
+    type: {
+      type: String,
+      default: "div",
+    },
+  },
+}
+</script>
