2ede737ab8fa6961e63995523efa8b627985968d
Felipe henrique <win2016_br@outlook.com>
2019-01-17 13:54:09 -0200

    Atualizacao

--- ds/src/patterns/Banner01.vue
+++ ds/src/patterns/Banner01.vue
@@ -1,30 +1,25 @@
 <template>
-  <Wrapper class="Wrapper1 row">
-    <Heading level="h3" class="Heading1 col-xs-12"
-      >Lorem ipsum sit amet arem ipsum sit amet'</Heading
-    >
-    <Wrapper class="Wrapper2 col-xs">
-      <Paragraph class="paragraph1">
-        Lorem ipsum dolor sit amet, consectetur adipiscing.
-      </Paragraph>
-      <Paragraph class="paragraph2">
-        Duis aute irure dolor in reprehenderit<br />in voluptate velit esse cillum dolore eu<br />fugiat
-        nulla pariatur. Excepteur sint<br />accaecat cupidatat
-      </Paragraph>
-      <button>Explorar Recursos</button>
-    </Wrapper>
-    <Wrapper class="Wrapper3 col-xs">
-      <img
-        src="https://lh4.googleusercontent.com/7BLk-Wo82esCj_VCPqlj6ONX6q4vdSw0Ui6mfL4g7Pdu-oLxU0K0Y1_CmZBW-tf-a8ANIwm87LVuTw=w1366-h657"
-      />
-    </Wrapper>
-  </Wrapper>
+  <div class="banner row">
+    <h1 class="col-xs-12">Lorem ipsum sit amet arem ipsum sit amet'</h1>
+    <div class="col-xs-12 col-sm-8 col-md-6 col-lg-6 cx-1">
+      <div class="start-xs ">
+        <p class="p1">Lorem ipsum dolor sit amet, consectetur adipiscing.</p>
+      </div>
+      <div class="start-xs ">
+        <p class="p2">
+          Duis aute irure dolor in reprehenderit<br />in voluptate velit esse cillum dolore eu<br />fugiat
+          nulla pariatur. Excepteur sint<br />accaecat cupidatat
+        </p>
+      </div>
+      <div class="start-xs"><button>Explorar Recursos</button></div>
+    </div>
+    <div class="col-xs-12 col-sm-8 col-md-6 col-lg-6 cx-2">
+      <img src="../assets/sem.imagens.png" />
+    </div>
+  </div>
 </template>
 
 <script>
-/**
- * Shows how to layout and structure a home page.
- */
 export default {
   name: "Banner01",
   status: "deprecated",
@@ -49,124 +44,54 @@ export default {
 
 <style lang="scss" scoped>
 @import "../../node_modules/flexboxgrid/dist/flexboxgrid.min.css";
-// Design Tokens with local scope
-$color-template-background: $color-white;
-$color-template-background-top: tint($color-template-background, 10%);
-$color-template-background-bottom: shade($color-template-background, 5%);
-$color-template-text: $color-white;
-$color-template-link: $color-accent-3;
-
-.Wrapper1 {
-  @include reset;
-  @include inset-space($space-m);
-  min-height: $space-xxl * 4;
-  background: $color-white;
-  position: relative;
+.banner {
+  margin: 20px 0px 50px 0px;
+}
+.cx-1 {
+  padding: 40px;
+}
+.cx-2 {
+  padding: 30px;
+}
+h1 {
+  text-align: center;
+  color: $color-primary-50;
+  font-size: 36px;
+}
+.p1 {
+  margin: 0px 0px 0px 30px;
+  padding: 0px;
+  font-size: 26px;
+  text-align: left;
+  color: $color-primary-40;
+}
+.p2 {
+  margin: 10px 0px 0px 30px;
+  padding: 0px;
+  font-size: 32px;
+  text-align: left;
+  color: $color-primary-50;
+}
+button {
+  margin: 20px 0px 0px 30px;
+  width: 50%;
+  height: 50px;
+  border-radius: 5px;
+  border: 0px;
+  font-size: 18px;
+  color: white;
+  background: $color-primary-60;
+  cursor: pointer;
   float: left;
 }
-.Heading1 {
-  text-align: center;
-  color: $color-primary-60;
-  margin-top: 40px;
-  padding-bottom: 0px;
+button:hover {
+  width: 51%;
+  height: 54px;
 }
-.Wrapper2 {
-  width: 100%;
-  margin: 0px;
-  padding: 0px;
-  text-align: left;
-}
-.Wrapper3 {
-  width: 100%;
-  margin: 0px;
-  padding: 0px;
-}
-.paragraph1 {
-  margin: 10px 0px 0px 120px;
-  padding: 0px;
-  color: $color-primary-40;
-  font-size: $size-l - 5;
-}
-.paragraph2 {
-  margin: 4px 0px 10px 120px;
-  padding: 0px;
-  color: $color-primary-50;
-  font-size: $size-l;
-}
-.Wrapper2 button {
-  margin: 0px 0px 0px 120px;
-  padding: 0px;
-  border: 0px;
-  border-radius: 6px;
-  color: $color-white;
-  background: $color-primary-50;
-  width: 40%;
-  height: 18%;
-}
-.Wrapper3 img {
-  margin: 4px 0px 0px 0px;
-  padding: 0px;
-  width: 60%;
-  border-radius: 15px;
-  text-align: center;
-}
-
-@media screen and(max-width: 1024px) {
-  .Wrapper1 {
-    @include reset;
-    @include inset-space($space-m);
-    min-height: $space-xxl * 4;
-    background: $color-white;
-    position: relative;
-    float: left;
-  }
-  .Heading1 {
-    text-align: center;
-    color: $color-primary-60;
-    margin-top: 40px;
-    padding-bottom: 0px;
-    font-size: $size-l + 4;
-  }
-  .Wrapper2 {
-    width: 100%;
-    margin: 0px;
-    padding: 0px;
-    text-align: left;
-  }
-  .Wrapper3 {
-    width: 100%;
-    margin: 0px;
-    padding: 0px;
-  }
-  .paragraph1 {
-    margin: 10px 0px 0px 40px;
-    padding: 0px;
-    color: $color-primary-40;
-    font-size: $size-m;
-  }
-  .paragraph2 {
-    margin: 4px 0px 10px 40px;
-    padding: 0px;
-    color: $color-primary-50;
-    font-size: $size-m + 5;
-  }
-  .Wrapper2 button {
-    margin: 0px 0px 0px 40px;
-    padding: 0px;
-    border: 0px;
-    border-radius: 6px;
-    color: $color-white;
-    background: $color-primary-50;
-    width: 50%;
-    height: 18%;
-  }
-  .Wrapper3 img {
-    margin: 10px 0px 0px 0px;
-    padding: 0px;
-    width: 80%;
-    border-radius: 15px;
-    text-align: center;
-  }
+img {
+  width: 80%;
+  height: 80%;
+  border-radius: 10px;
 }
 </style>
 
--- ds/src/patterns/TopBanner.vue
+++ ds/src/patterns/TopBanner.vue
@@ -1,5 +1,5 @@
 <template>
-  <carousel class="index" :data="data"></carousel>
+  <div class="Slide"><carousel class="index" :data="data"></carousel></div>
 </template>
 
 <script>
@@ -39,65 +39,8 @@ export default {
 </script>
 
 <style lang="scss" scoped>
-// Design Tokens with local scope
-$color-template-background: $color-black;
-$color-template-background-top: tint($color-template-background, 10%);
-$color-template-background-bottom: shade($color-template-background, 5%);
-$color-template-text: $color-white;
-$color-template-link: $color-accent-3;
-.index {
-  width: 96%;
-  margin-left: 2%;
-  margin-right: 2%;
-}
-.component {
-  margin: 0px;
-}
-.heading {
-  color: $color-primary-50;
-  font-size: $size-xl;
-  text-align: center;
-  margin-top: $size-xl;
-}
-.paragraph1 {
-  color: rgb(0, 107, 139);
-  margin-bottom: 8px;
-  font-size: $size-s;
-}
-.paragraph2 {
-  color: rgb(0, 139, 139);
-  margin-bottom: 8px;
-}
-.text-link {
-  color: $color-template-link;
-}
-.wrapper {
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  max-width: $space-xxl * 4.5;
-  transform: translateX(-50%) translateY(-50%);
-  position: absolute;
-  left: 30%;
-  top: 50%;
-}
-a {
-  font-family: $font-text;
-  color: $color-primary;
-  text-decoration: underline;
-}
-.active {
-  color: $color-base-40;
-}
-.button {
-  margin-top: $size-m;
-}
-@media screen and(max-width: 1024px) {
-  .index {
-    width: 96%;
-    height: 80%;
-    margin-left: 2%;
-    margin-right: 2%;
-  }
+.Slide {
+  width: 100%;
 }
 </style>
 
