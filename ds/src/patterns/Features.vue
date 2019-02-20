016dfd72ae829c8b51f1a40911e1bdea50ee26bc
Carlos Elias <carlos.elias.10@hotmail.com>
2019-02-20 18:02:38 -0300

    changes in website

--- ds/src/patterns/Features.vue
+++ ds/src/patterns/Features.vue
@@ -8,7 +8,7 @@
           <li><a class="t" href="javascript:void(0)" @click="tab = 2">Vendas</a></li>
           <li><a class="t" href="javascript:void(0)" @click="tab = 3">Logística</a></li>
           <li><a class="t" href="javascript:void(0)" @click="tab = 4">Financeiro</a></li>
-          <li><a class="t" href="javascript:void(0)" @click="tab = 5">Fiscal</a></li>
+          <li><a class="t" href="javascript:void(0)" @click="tab = 5">Relatórios</a></li>
         </ul>
       </div>
     </div>
@@ -83,12 +83,11 @@
           <div style="position:absolute;top:-2px;left:80px;">
             <img src="../assets/adv-icon.png" />
           </div>
-          <div class="g1">Pesquisa inteligente, rápida e offline</div>
+          <div class="g1">#</div>
           <div style="padding-left:0px 25px 0px 30px;">
             <p class="text">
-              Pesquisa de clientes com campo único, o que você digita é analisado na hora; como
-              no Google, a parte complicada fica com a gente, não precisa um monte de campos para
-              encontrar o cliente cadastrado ou incluir um novo.
+            Retenha clientes e evite a inadimplência, visualize o histórico de compras, 
+            crédito disponível e vendas a prazo no momento da negociação.
             </p>
           </div>
         </div>
@@ -96,11 +95,12 @@
           <div style="position:absolute;top:-2px;left:80px;">
             <img src="../assets/adv-icon.png" />
           </div>
-          <div class="g1">Tudo sincronizado em uma tela</div>
+          <div class="g1">#</div>
           <div>
             <p class="text">
-              Múltiplos atendimentos, múltiplos endereços, múltiplos recebimentos e muitos outros múltiplos funcionando na nuvem e fora dela quando não há
-              conexão com a internet.
+               Precisa alterar dados de um atendimento, como a forma de pagamento 
+               no pedido finalizado? Faça isso na hora, sem precisar cancelar a venda 
+               e perder tempo.
             </p>
           </div>
         </div>
@@ -108,12 +108,11 @@
           <div style="position:absolute;top:-2px;left:80px;">
             <img src="../assets/adv-icon.png" />
           </div>
-          <div class="g1">Destino identificado</div>
+          <div class="g1">#</div>
           <div>
             <p class="text">
-              Quando o cliente liga, a busca pelo endereço e histórico de compras é automática.
-              A linha de destino pode ser nomeada, permitindo um atendimento personalizado e
-              medição das vendas por telefone divulgado.
+              Atendentes ou vendedores precisam se comunicar rapidamente durante um atendimento?
+              É só iniciar um chat em tempo real dentro do pedido! 
             </p>
           </div>
         </div>
@@ -121,12 +120,12 @@
           <div style="position:absolute;top:-2px;left:80px;">
             <img src="../assets/adv-icon.png" />
           </div>
-          <div class="g1">Controle em uma tela</div>
+          <div class="g1">#</div>
           <div>
             <p class="text">
-              Os pedidos no painel de atendimento são rapidamente editáveis sem precisar 
-              mudar de tela, tudo é sincronizado e salvo em tempo real. Cadastre clientes e 
-              realize as vendas mesmo sem internet. 
+              Durante o processo de venda assinale poucos campos que podem ser 
+              obrigatórios ou não para gerar indicadores importantes sobre seus canais de 
+              divulgação e grupos de cliente.
             </p>
           </div>
         </div>
@@ -143,12 +142,12 @@
           <div style="position:absolute;top:-2px;left:80px;">
             <img src="../assets/adv-icon.png" />
           </div>
-          <div class="g1">Pesquisa inteligente, rápida e offline</div>
+          <div class="g1">#</div>
           <div style="padding-left:0px 25px 0px 30px;">
             <p class="text">
-              Pesquisa de clientes com campo único, o que você digita é analisado na hora; como
-              no Google, a parte complicada fica com a gente, não precisa um monte de campos para
-              encontrar o cliente cadastrado ou incluir um novo.
+               Direcione os pedidos de vendas para o aplicativo do vendedor com um 
+               clique ou ative o encaminhamento automático e gerencie o tempo e o 
+               sucesso das entregas
             </p>
           </div>
         </div>
@@ -156,11 +155,11 @@
           <div style="position:absolute;top:-2px;left:80px;">
             <img src="../assets/adv-icon.png" />
           </div>
-          <div class="g1">Tudo sincronizado em uma tela</div>
+          <div class="g1">#</div>
           <div>
             <p class="text">
-              Múltiplos atendimentos, múltiplos endereços, múltiplos recebimentos e muitos outros múltiplos funcionando na nuvem e fora dela quando não há
-              conexão com a internet.
+              Qual é prioridade das entregas? Os pedidos possuem indicação de 
+              urgência por escala de cor, minutos transcorridos e pedidos agendados
             </p>
           </div>
         </div>
@@ -168,12 +167,12 @@
           <div style="position:absolute;top:-2px;left:80px;">
             <img src="../assets/adv-icon.png" />
           </div>
-          <div class="g1">Destino identificado</div>
+          <div class="g1">#</div>
           <div>
             <p class="text">
-              Quando o cliente liga, a busca pelo endereço e histórico de compras é automática.
-              A linha de destino pode ser nomeada, permitindo um atendimento personalizado e
-              medição das vendas por telefone divulgado.
+              Desenhe no mapa suas regiões de atendimento, acompanhe o 
+              deslocamento de seus agentes de logística e a proximidade com os clientes, 
+              trace rotas para entender o histórico das entregas
             </p>
           </div>
         </div>
@@ -181,12 +180,11 @@
           <div style="position:absolute;top:-2px;left:80px;">
             <img src="../assets/adv-icon.png" />
           </div>
-          <div class="g1">Controle em uma tela</div>
+          <div class="g1">#</div>
           <div>
             <p class="text">
-              Os pedidos no painel de atendimento são rapidamente editáveis sem precisar 
-              mudar de tela, tudo é sincronizado e salvo em tempo real. Cadastre clientes e 
-              realize as vendas mesmo sem internet. 
+              Controle o estoque e itens em trânsito por entrada manual ou QR Code. 
+              Atualize o nível de estoque pelas compras e fechamento das vendas nos aplicativos.
             </p>
           </div>
         </div>
@@ -203,12 +201,12 @@
           <div style="position:absolute;top:-2px;left:80px;">
             <img src="../assets/adv-icon.png" />
           </div>
-          <div class="g1">Pesquisa inteligente, rápida e offline</div>
+          <div class="g1">#</div>
           <div style="padding-left:0px 25px 0px 30px;">
             <p class="text">
-              Pesquisa de clientes com campo único, o que você digita é analisado na hora; como
-              no Google, a parte complicada fica com a gente, não precisa um monte de campos para
-              encontrar o cliente cadastrado ou incluir um novo.
+              A conferência das entregas é feita rapidamente pela atendente via 
+              aplicativo móvel do vendedor e painel web, os valores recebidos são 
+              recolhidos e posteriormente conferidos na conciliação financeira
             </p>
           </div>
         </div>
@@ -216,11 +214,12 @@
           <div style="position:absolute;top:-2px;left:80px;">
             <img src="../assets/adv-icon.png" />
           </div>
-          <div class="g1">Tudo sincronizado em uma tela</div>
+          <div class="g1">#</div>
           <div>
             <p class="text">
-              Múltiplos atendimentos, múltiplos endereços, múltiplos recebimentos e muitos outros múltiplos funcionando na nuvem e fora dela quando não há
-              conexão com a internet.
+              Cada operador com perfil de vendas tem sua própria conta/carteira. 
+              Sendo possível realizar conferência de entregas, sangria de caixa e 
+              lançamento de despesas de forma simplificada
             </p>
           </div>
         </div>
@@ -228,30 +227,18 @@
           <div style="position:absolute;top:-2px;left:80px;">
             <img src="../assets/adv-icon.png" />
           </div>
-          <div class="g1">Destino identificado</div>
+          <div class="g1">#</div>
           <div>
             <p class="text">
-              Quando o cliente liga, a busca pelo endereço e histórico de compras é automática.
-              A linha de destino pode ser nomeada, permitindo um atendimento personalizado e
-              medição das vendas por telefone divulgado.
-            </p>
-          </div>
-        </div>
-        <div class="col-md-6 c" style="position:relative">
-          <div style="position:absolute;top:-2px;left:80px;">
-            <img src="../assets/adv-icon.png" />
-          </div>
-          <div class="g1">Controle em uma tela</div>
-          <div>
-            <p class="text">
-              Os pedidos no painel de atendimento são rapidamente editáveis sem precisar 
-              mudar de tela, tudo é sincronizado e salvo em tempo real. Cadastre clientes e 
-              realize as vendas mesmo sem internet. 
+              Analisar o Fluxo de caixa e o Demostrativo de Resultados Operacional é 
+              fundamental para o sucesso do negócio, cuidamos para que esses dados 
+              sejam apresentados de forma de objetiva. Você só precisa fazer os 
+              lançamentos de despesas de forma precisa, já que as vendas são 
             </p>
           </div>
         </div>
       </div>
-    </div>
+      </div>
     <div v-if="tab === 5" style="width: 100%">
       <div style="position:absolute;top:0px;left:200px;">
         <h2 class="p" style="font-size:24px;color: #276E73;">
@@ -263,12 +250,11 @@
           <div style="position:absolute;top:-2px;left:80px;">
             <img src="../assets/adv-icon.png" />
           </div>
-          <div class="g1">Pesquisa inteligente, rápida e offline</div>
+          <div class="g1">#</div>
           <div style="padding-left:0px 25px 0px 30px;">
             <p class="text">
-              Pesquisa de clientes com campo único, o que você digita é analisado na hora; como
-              no Google, a parte complicada fica com a gente, não precisa um monte de campos para
-              encontrar o cliente cadastrado ou incluir um novo.
+              Crie ações de marketing associadas a regiões, canais de vendas e grupo de 
+              clientes e se concentre onde houver maior retorno sobre o investimento 
             </p>
           </div>
         </div>
@@ -276,11 +262,11 @@
           <div style="position:absolute;top:-2px;left:80px;">
             <img src="../assets/adv-icon.png" />
           </div>
-          <div class="g1">Tudo sincronizado em uma tela</div>
+          <div class="g1">#</div>
           <div>
             <p class="text">
-              Múltiplos atendimentos, múltiplos endereços, múltiplos recebimentos e muitos outros múltiplos funcionando na nuvem e fora dela quando não há
-              conexão com a internet.
+              Relatórios e indicadores precisos por vendedor, veículos, posição do estoque 
+              nas bases, em trânsito, mcmm
             </p>
           </div>
         </div>
@@ -288,25 +274,12 @@
           <div style="position:absolute;top:-2px;left:80px;">
             <img src="../assets/adv-icon.png" />
           </div>
-          <div class="g1">Destino identificado</div>
+          <div class="g1">#</div>
           <div>
             <p class="text">
-              Quando o cliente liga, a busca pelo endereço e histórico de compras é automática.
-              A linha de destino pode ser nomeada, permitindo um atendimento personalizado e
-              medição das vendas por telefone divulgado.
-            </p>
-          </div>
-        </div>
-        <div class="col-md-6 c" style="position:relative">
-          <div style="position:absolute;top:-2px;left:80px;">
-            <img src="../assets/adv-icon.png" />
-          </div>
-          <div class="g1">Controle em uma tela</div>
-          <div>
-            <p class="text">
-              Os pedidos no painel de atendimento são rapidamente editáveis sem precisar 
-              mudar de tela, tudo é sincronizado e salvo em tempo real. Cadastre clientes e 
-              realize as vendas mesmo sem internet. 
+              No GestaON você pode mapear todos os clientes em potenciais de uma 
+              região e comparar com as vendas realizadas, fica fácil saber para onde 
+              direcionar sua próxima ação de marketing
             </p>
           </div>
         </div>
