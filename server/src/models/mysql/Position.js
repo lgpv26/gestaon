8f9eadb4e6bf3a86a9d2c30aed8313abb30b4e12
Mailon Ruan <maailon@msn.com>
2019-03-21 15:07:36 -0300

    Some Models mysql removed

--- server/src/models/mysql/Position.js
+++ server/src/models/mysql/Position.js
@@ -3,8 +3,8 @@ import _ from 'lodash'
 
 module.exports = {
     defineModel: (server) => {
-        const TIMESTAMP = require('sequelize-mysql-timestamp')(server.sequelize);
-        const modelName = 'RequestPaymentBill';
+        const TIMESTAMP = require('sequelize-mysql-timestamp')(server.sequelize)
+        const modelName = 'Position'
         return {
             name: modelName,
             instance: server.sequelize.define(_.camelCase(modelName), {
@@ -13,11 +13,15 @@ module.exports = {
                     primaryKey: true,
                     autoIncrement: true
                 },
-                requestPaymentId: {
-                    type: Sequelize.INTEGER
+                deviceId: {
+                    type: Sequelize.STRING
                 },
-                deadlineDatetime: {
-                    type: TIMESTAMP
+                position: {
+                    type: Sequelize.GEOMETRY('POINT', 4326),
+                    allowNull: false
+                },
+                obs: {
+                    type: Sequelize.STRING,
                 },
                 dateUpdated: {
                     type: TIMESTAMP
@@ -27,21 +31,23 @@ module.exports = {
                 },
                 dateRemoved: {
                     type: TIMESTAMP
+                },
+                status: {
+                    type: Sequelize.STRING,
+                    defaultValue: 'activated'
                 }
             }, {
-                tableName: 'request_payment_bill',
+                tableName: "position",
                 timestamps: true,
                 updatedAt: 'dateUpdated',
                 createdAt: 'dateCreated',
                 deletedAt: 'dateRemoved',
-                freezeTableName: true,
-                paranoid: true
+                paranoid: true,
+                freezeTableName: true
             })
         }
     },
-    postSettings: ({RequestPaymentBill, RequestPayment, RequestPaymentBillPayment, Request}) => {
-        RequestPaymentBill.belongsTo(RequestPayment, {as: 'requestPayments', foreignKey: 'requestPaymentId'})
-        RequestPaymentBill.belongsToMany(Request, {through: RequestPayment, as: 'requests', foreignKey: 'requestId'})
-        RequestPaymentBill.hasMany(RequestPaymentBillPayment, {as: 'requestPaymentBillPayments', foreignKey: 'requestPaymentBillId'})    
+    postSettings: ({Position, Device}) => {
+        Position.belongsTo(Device, {as: 'device', foreignKey: 'deviceId' })
     }
 }
